245c06b40ec1867f74c96b67e23567c51d32a0d9
utilisateur <utilisateur@PF1DAPSH>
2024-06-13 18:57:26 +0200

    Changement du type du input pour le mot de passe

--- projet/app/view/connexion/viewErrorConnexion.php
+++ projet/app/view/connexion/viewErrorConnexion.php
@@ -11,12 +11,12 @@ require ($root . '/app/view/fragment/fragmentPatrimoineHeader.html');
       include $root . '/app/view/fragment/fragmentPatrimoineMenu.html';
       include $root . '/app/view/fragment/fragmentPatrimoineJumbotron.html';
     ?> 
-      <h3>Login ou mot de passe incorrect</h3>
+      <h5>Login ou mot de passe incorrect</h5>
     <form role="form" method='get' action='router1.php'>
       <div class="form-group">
         <input type="hidden" name='action' value='patrimoineConnected'>        
         <label class='w-25' for="login">Login : </label><input type="text" name='login' size='40' required> <br/>                          
-        <label class='w-25' for="password">Password : </label><input type="text" name='password' size='40' required> <br/> 
+        <label class='w-25' for="password">Password : </label><input type="password" name='password' size='40' required> <br/> 
       </div>
       <p/>
        <br/> 
