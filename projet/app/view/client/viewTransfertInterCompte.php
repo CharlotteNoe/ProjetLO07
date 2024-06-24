b3cf953b22191ee520d17a626f2fd246ed20f269
utilisateur <utilisateur@PF1DAPSH>
2024-06-24 14:23:49 +0200

    Transfert InterCompte

--- projet/app/view/client/viewTransfertInterCompte.php
+++ projet/app/view/client/viewTransfertInterCompte.php
@@ -21,20 +21,20 @@ require ($root . '/app/view/fragment/fragmentPatrimoineHeader.html');
     <form role="form" method='get' action='router1.php' class="mt-4 p-2 text-white bg-secondary rounded">
         <div class="form-group">
             <input type="hidden" name='action' value='clientTransfertInterCompteFait'>
-            <label for="compteMoins">Sélectionnez le compte d'où part l'argent : </label> <select class="form-control" id='compteMoins' name='compteMoins' style="width: 150px">
+            <label for="compteMoins">Sélectionnez le compte d'où part l'argent : </label> <select class="form-control" id='compteMoins' name='compteMoins' required style="width: 300px">
                 <?php
                 foreach ($results as $compte) {
-                    $id_compte=$compte->getId();
-                    $label_compte=$compte->getLabel();
+                    $id_compte=htmlspecialchars($compte['id']);
+                    $label_compte=htmlspecialchars($compte['compte_label']);
                     echo ("<option value='$id_compte'>$label_compte</option>");
                 }
                 ?>
             </select>
-            <label for="comptePlus">Sélectionnez le compte où arrive l'argent : </label> <select class="form-control" id='comptePlus' name='comptePlus' style="width: 150px">
+            <label for="comptePlus">Sélectionnez le compte où arrive l'argent : </label> <select class="form-control" id='comptePlus' name='comptePlus' required style="width: 300px">
                 <?php
                 foreach ($results as $compte) {
-                    $id_compte=$compte->getId();
-                    $label_compte=$compte->getLabel();
+                    $id_compte=htmlspecialchars($compte['id']);
+                    $label_compte=htmlspecialchars($compte['compte_label']);
                     echo ("<option value='$id_compte'>$label_compte</option>");
                 }
                 ?>
